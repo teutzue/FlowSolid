31fd90a749e0dd8de1d8841faf6af2138458494f
Aleksandar <sashoterziev111@gmail.com>
2014-11-11 10:18:30 +0100

    FlowSolidver1

---
+++ src/flowsolid/Control.java
@@ -0,0 +1,29 @@
+//package flowsolid;
+//
+//public class Control implements WordPairControlInterface{
+//        
+//    void add(String question, String answer){
+//        
+//    }
+//    int size(){
+//        
+//    }
+//    String getRandomQuestion(){
+//    
+//    }
+//    boolean checkGuess(String question, String quess){
+//    
+//    }
+//    String lookup(String question){
+//    
+//    }
+//    boolean load(String filename){
+//    
+//    }
+//    boolean save(String filename){
+//    
+//    }
+//    void clear(){
+//    
+//    }
+//}
